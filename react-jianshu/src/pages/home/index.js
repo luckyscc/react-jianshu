dbfdca7cf62e5d71f05cd75b40a0e0827d52c5fd
songjiwei <939346832@qq.com>
2018-12-16 21:43:09 +0800

    文章

---
+++ react-jianshu/src/pages/home/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Topic from './components/Topic';
+import List from './components/List';
+import Recommend from './components/Recommend';
+import Writer from './components/Writer';
+import { actionCreators } from './store';
+import {
+    HomeWrapper,
+    HomeLeft,
+    HomeRight
+} from './style';
+
+class Home extends Component {
+    render() {
+        return (
+            <HomeWrapper>
+                <HomeLeft>
+                    <img className="banner-img" src="//upload.jianshu.io/admin_banners/web_images/4581/14d959ff757f21616baa933fd3bdbfcf52fd512a.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540" alt=""/>
+                    <Topic />
+                    <List />
+                </HomeLeft>
+                <HomeRight>
+                    <Recommend />
+                    <Writer />
+                </HomeRight>
+            </HomeWrapper>
+        )
+    }
+
+    componentDidMount() {
+        this.props.changeHomeData()
+    }
+}
+
+const mapDispatch = (dispatch) => ({
+    changeHomeData() {
+        const action = actionCreators.getHomeInfo();
+        dispatch(action);
+    }
+})
+export default connect(null, mapDispatch)(Home);
---
+++ react-jianshu/src/pages/home/style.js
@@ -0,0 +1,102 @@
+import styled from 'styled-components';
+
+export const HomeWrapper = styled.div`
+    width: 960px;
+    margin: 0 auto;
+    overflow: hidden;
+`;
+
+export const HomeLeft = styled.div`
+    margin-left: 15px;
+    padding-top: 30px;
+    width: 625px;
+    float: left;
+    .banner-img {
+        width: 625px;
+        height: 240px;
+    }
+`;
+
+export const HomeRight = styled.div`
+    width: 280px;
+    float: right;
+`;
+
+export const TopicWrapper = styled.div`
+    padding: 20px 0 10px 0;
+    overflow: hidden;
+    margin-left: -18px;
+    border-bottom: 1px solid #dcdcdc;
+`;
+
+export const TopicItem = styled.div`
+    float: left;
+    padding-right: 10px;
+    margin-left: 18px;
+    margin-bottom: 18px;
+    background: #f7f7f7;
+    height: 32px;
+    line-height: 32px;
+    border: 1px solid #dcdcdc;
+    border-radius: 4px;
+    font-size: 14px;
+    .topic-pic {
+        float: left;
+        margin-right: 10px;
+        disolay: block;
+        width: 32px;
+        height: 32px;
+        border-radius: 2px 0 0 2px;
+    }
+`;
+
+export const ListItem = styled.div`
+    padding: 20px 0;
+    border-bottom: 1px solid #dcdcdc;
+    overflow: hidden;
+    .pic {
+        width: 125px;
+        height: 100px;
+        disolay: block;
+        float: right;
+        border-radius: 10px;
+    }
+`;
+
+export const ListInfo = styled.div`
+    width: 500px;
+    float: left;
+    .title {
+        line-height: 27px;
+        font-size: 18px;
+        font-weight: bold;
+        color: #333;
+    }
+    .desc {
+        line-heiht:24px;
+        color: #999;
+        font-size: 13px;
+        margin-top: 7px;
+    }
+`;
+
+export const RecommendWrapper = styled.div`
+    width: 280px;
+    margin: 30px 0;
+`;
+
+export const RecommendItem = styled.div`
+    width: 280px;
+    height: 50px;
+    background: url(${(props) => props.imgUrl});
+    background-size: contain;
+`;
+
+export const WriterWrapper = styled.div`
+    width: 278px;
+    border: 1px solid #dcdcdc;
+    border-radius: 3px;
+    height: 300px;
+    line-height: 300px;
+    text-align: center;
+`;
